1a34c8e8c7849567b4ecf95eef7a4b1f2df9aa39
TheMrNomis <n0m1s@homnomnom.fr>
2015-08-13 17:44:44 +0200

    BDD base implementation

--- BDD.cpp
+++ BDD.cpp
@@ -1,7 +1,50 @@
 #include "BDD.h"
 
-BDD::BDD(QObject *parent) : QObject(parent)
+BDD::BDD(QString const& bddLocation, QObject *parent) :
+  QObject(parent),
+  m_bdd(QSqlDatabase::addDatabase("QSQLITE"))
 {
+  qDebug() << "Loading database" << bddLocation;
 
+  m_bdd.setHostName("localhost");
+  m_bdd.setDatabaseName(bddLocation);
+
+  if (!m_bdd.open())
+  {
+    qDebug() << "Error opening the database";
+    //TODO
+  }
 }
 
+QList<QString> BDD::listAllArtists()
+{
+  QList<QString> artists;
+
+  QSqlQuery query = QSqlQuery("SELECT artist_name FROM artist", m_bdd);
+  while(query.next())
+    artists.append(query.value(0).toString());
+
+  return artists;
+}
+
+QList<QString> BDD::listAllAlbums()
+{
+  QList<QString> albums;
+
+  QSqlQuery query = QSqlQuery("SELECT album_name FROM album", m_bdd);
+  while (query.next())
+    albums.append(query.value(0).toString());
+
+  return albums;
+}
+
+QList<QString> BDD::listAllSongs()
+{
+  QList<QString> songs;
+
+  QSqlQuery query = QSqlQuery("SELECT song_name FROM song", m_bdd);
+  while (query.next())
+    songs.append(query.value(0).toString());
+
+  return songs;
+}
--- BDD.h
+++ BDD.h
@@ -1,17 +1,27 @@
 #ifndef BDD_H
 #define BDD_H
 
+#include <QDebug>
 #include <QObject>
+#include <QtSql>
+#include <QList>
 
 class BDD : public QObject
 {
   Q_OBJECT
 public:
-  explicit BDD(QObject *parent = 0);
+  explicit BDD(QString const& bddLocation, QObject *parent = 0);
+
+  QList<QString> listAllArtists();
+  QList<QString> listAllAlbums();
+  QList<QString> listAllSongs();
 
 signals:
 
 public slots:
+
+private:
+  QSqlDatabase m_bdd;
 };
 
 #endif // BDD_H
