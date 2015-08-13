987c559407c22e6dbcdbd976ebc747895fa42ba3
TheMrNomis <n0m1s@homnomnom.fr>
2015-08-13 17:45:38 +0200

    Modified main to load BDD and Parameters

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <QCoreApplication>
+#include <QMediaPlayer>
+#include <QDebug>
+#include <taglib/tag.h>
+
+#include "Parameters.h"
+#include "BDD.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Parameters* param = new Parameters(argc, argv);
+    BDD* bdd = new BDD(param->getDbLocation());
+
+    delete bdd;
+    delete param;
 
     return a.exec();
 }
